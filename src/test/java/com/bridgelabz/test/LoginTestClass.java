7c5f3fa8979df832faef54ccceea016fb89d0b21
Surabhi951 <surabhibhagat95@gmail.com>
2023-03-29 15:28:14 +0530

    Automating facebook login using keyword driven framework

---
+++ src/test/java/com/bridgelabz/test/LoginTestClass.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.test;
+
+import com.bridgelabz.keywordengine.KeywordDrivenFrameworkEngine;
+import org.testng.annotations.Test;
+
+public class LoginTestClass {
+
+    public KeywordDrivenFrameworkEngine keywordDrivenFrameworkEngine;
+
+    @Test
+    public void login_Using_KeywordDriven_Test() throws InterruptedException {
+        keywordDrivenFrameworkEngine = new KeywordDrivenFrameworkEngine();
+        keywordDrivenFrameworkEngine.start_Execution("Sheet1");
+        Thread.sleep(2000);
+    }
+}
